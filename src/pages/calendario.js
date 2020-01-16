ea85342a98acefd64b7b6be58d4d4065e7d26e93
tetsuo shima <primaveraentalca@gmail.com>
2020-01-16 19:30:50 -0300

    styled p

--- src/pages/calendario.js
+++ src/pages/calendario.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { Styled, jsx } from "theme-ui"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -8,12 +8,24 @@ import SEO from "../components/seo"
 const CalendarioPage = ({ location }) => (
   <Layout location={location.pathname}>
     <SEO title="calendario fotográfico" />
-    Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis facilisis,
-    ultrices ad diam torquent scelerisque a lacinia suspendisse nec, auctor
-    hendrerit posuere magna hac laoreet orci parturient. Tempor sociis euismod
-    quisque montes iaculis vitae nisi eu arcu sagittis velit cursus, vivamus est
-    tincidunt tortor in volutpat augue eget magnis primis{" "}
-    <Link to="/calendario/2019">octubre 2019</Link>.
+    <Styled.p>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis
+      facilisis, ultrices ad diam torquent scelerisque a lacinia suspendisse
+      nec, auctor hendrerit posuere magna hac laoreet orci parturient. Tempor
+      sociis euismod quisque montes iaculis vitae nisi eu arcu sagittis velit
+      cursus, vivamus est tincidunt tortor in volutpat augue eget magnis primis{" "}
+      <Link
+        to="/calendario/2019"
+        sx={{
+          textDecoration: `none`,
+          color: "secondary",
+        }}
+      >
+        octubre 2019
+      </Link>
+      .
+    </Styled.p>
+    .
   </Layout>
 )
 
