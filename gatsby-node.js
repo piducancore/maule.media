c86c10680b8a965ef87073b6ae8fae8f11a90bd5
Rodrigo Moreira <hola@piducan.dev>
2021-01-02 01:24:28 -0300

    migrate to dayjs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,8 @@
 const path = require(`path`)
-const moment = require("moment")
-const localization = require("moment/locale/es")
-
-moment.updateLocale("es", localization)
+const dayjs = require("dayjs")
+const customParseFormat = require("dayjs/plugin/customParseFormat")
+dayjs.locale("es") // use Spanish locale globally
+dayjs.extend(customParseFormat)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -26,33 +26,46 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  result.data.allCloudinaryMedia.nodes.forEach(({ context }) => {
-    const date = moment(context.custom.createDate, "YYYY:MM:DD")
-    const day = date.format("YYYY:MM:DD")
-    const month = date.format("YYYY:MM")
-    const year = date.format("YYYY")
-    createPage({
-      path: `/archivo/${day.replace(/:/g, "/")}`,
-      component: path.resolve(`./src/templates/day.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        day: `/^${day}/`,
-      },
+  result.data.allCloudinaryMedia.nodes
+    .reduce((unique, o) => {
+      if (
+        !unique.some(
+          obj =>
+            obj.context.custom.createDate.split(" ")[0] ===
+            o.context.custom.createDate.split(" ")[0]
+        )
+      ) {
+        unique.push(o)
+      }
+      return unique
+    }, [])
+    .forEach(({ context }) => {
+      const date = dayjs(context.custom.createDate.split(" ")[0], "YYYY:MM:DD")
+      const day = date.format("YYYY:MM:DD")
+      const month = date.format("YYYY:MM")
+      const year = date.format("YYYY")
+      createPage({
+        path: `/archivo/${day.replace(/:/g, "/")}`,
+        component: path.resolve(`./src/templates/day.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          day: `/^${day}/`,
+        },
+      })
+      createPage({
+        path: `/archivo/${month.replace(/:/g, "/")}`,
+        component: path.resolve(`./src/templates/month.js`),
+        context: {
+          month: `/^${month}/`,
+        },
+      })
+      createPage({
+        path: `/archivo/${year.replace(/:/g, "/")}`,
+        component: path.resolve(`./src/templates/year.js`),
+        context: {
+          year: `/^${year}/`,
+        },
+      })
     })
-    createPage({
-      path: `/archivo/${month.replace(/:/g, "/")}`,
-      component: path.resolve(`./src/templates/month.js`),
-      context: {
-        month: `/^${month}/`,
-      },
-    })
-    createPage({
-      path: `/archivo/${year.replace(/:/g, "/")}`,
-      component: path.resolve(`./src/templates/year.js`),
-      context: {
-        year: `/^${year}/`,
-      },
-    })
-  })
 }
